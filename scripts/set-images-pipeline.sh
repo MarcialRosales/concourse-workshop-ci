615d630d5943eacdfe254ba82926badd14a71a99
Marcial Rosales <mrosales@pivotal.io>
2017-07-28 08:46:21 +0200

    Add pipeline that builds docker images including the set-images-pipeline.sh script to bootstrap it

---
+++ scripts/set-images-pipeline.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+
+syntax() {
+  echo "Usage: set-images-pipeline.sh concourse-target pipeline"
+}
+
+PIPELINE_DIR=$(dirname "$0")/..
+
+if [ "$#" -ne 2 ]; then
+  syntax
+  exit 1
+fi
+
+FLY_TARGET=$1
+PIPELINE=$2
+CREDENTIALS=credentials.yml
+SECRETS=secrets.yml
+
+
+tmp=$(mktemp $TMPDIR/pipeline.XXXXXX.yml)
+trap 'rm $tmp' EXIT
+
+PIPELINE_FILES="$PIPELINE_DIR/ci/images/pipeline.yml"
+
+echo "Generating $PIPELINE pipeline ..."
+spruce merge --prune meta --prune pipeline --prune app --prune deployment $PIPELINE_FILES $CREDENTIALS $SECRETS > $tmp
+
+echo "Setting $PIPELINE pipeline in Concourse ..."
+fly -t "$FLY_TARGET" set-pipeline -p "$PIPELINE" -c $tmp
