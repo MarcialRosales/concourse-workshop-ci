85927cf62813d6b914164f695b0a96b19c63eb26
Marcial Rosales <mrosales@pivotal.io>
2017-07-28 11:55:47 +0200

    Extract terraform folder from artifact

--- tasks/terraform.sh
+++ tasks/terraform.sh
@@ -2,6 +2,22 @@
 
 env | grep TF_VAR 
 
-cd source-code/deployments
+cd artifact
+ARTIFACT=`ls *`
 
+function tar {
+    tar -xvf $ARTIFACT    
+}
+
+case ${ARTIFACT_TYPE} in 
+tar) 
+    tar
+    ;;
+*)
+    echo $"Expected artifact type : tar"      
+    exit 1
+ 
+esac
+
+cd terraform 
 terraform plan
